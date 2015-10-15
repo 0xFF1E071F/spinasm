fc3c8cccda5e3932c5f6561f40d5746c3a0cfb0f
ner0x652 <cornel.punga@gmail.com>
2015-10-15 16:10:39 -0400

    quadratic formula asm code

--- quadt.asm
+++ quadt.asm
@@ -13,3 +13,52 @@ segment .text
     global  quadratic
 
 quadratic:
+    push    ebp
+    mov     ebp, esp
+    sub     esp, 16
+    push    ebx
+
+    fild    word [MinusFour]
+    fild    a
+    fild    c
+    fmulp   st1
+    fmulp   st1
+    fld     b
+    fld     b
+    fmulp   st1
+    faddp   st1
+    ftst
+    fstsw   ax
+    sahf
+    jb      no_real_sol
+    fsqrt
+    fstp    disc
+    fld1
+    fld     a
+    fscale  
+    fdivp   st1
+    fst     one2a
+    fld     b
+    fld     disc
+    fsubrp  st1
+    fmulp   st1
+    mov     ebx, root1
+    fstp    qword [ebx]
+    fld     b
+    fld     disc
+    fchs
+    fsubrp  st1
+    fmul    one2a
+    mov     ebx, root2
+    fstp    qword [ebx]
+    mov     eax, 1
+    jmp     short quit
+
+no_real_sol:
+    mov     eax, 0
+
+quit:
+    pop     ebx
+    mov     esp, ebp
+    pop     ebp
+    ret
